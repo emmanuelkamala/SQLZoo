1b1830069fd4a4f19522526cf4a267c786822caa
emmanuelkamala <49253728+emmanuelkamala@users.noreply.github.com>
2019-07-31 18:14:15 +0300

    added a question result on tutorial 9

--- Tutorial_9_Self_join.sql
+++ Tutorial_9_Self_join.sql
@@ -69,6 +69,20 @@ INNER JOIN stops AS stopb ON b.stop = stopb.id
 WHERE stopa.name ='Craiglockhart'
 AND a.company ='LRT'
 
+-- Question 10
+SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
+            AND  stopb.name = stopc.name
+ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num; 
+
+
 
 -- QUIZ
 1. C
