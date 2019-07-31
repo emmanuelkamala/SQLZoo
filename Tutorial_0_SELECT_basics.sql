158f81c6f1f3b7f9a2798f951c3b7b28deb76a1f
emmanuelkamala <49253728+emmanuelkamala@users.noreply.github.com>
2019-07-31 11:29:09 +0300

    modified naming of files and added more quiz results

--- Tutorial_0_SELECT_basics.sql
+++ Tutorial_0_SELECT_basics.sql
@@ -1,17 +1,19 @@
 -- Question 1
-
-SELECT population FROM world
-  WHERE name = 'Germany'
+SELECT population
+FROM world
+WHERE name = 'Germany'
 
 -- Question 2
-
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+SELECT name, population
+FROM world
+WHERE name
+IN ('Sweden', 'Norway', 'Denmark')
 
 -- Question 3
-
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
+SELECT name, area
+FROM world
+WHERE area
+BETWEEN 200000 AND 250000
 
 -- QUIZ
 1. C
--- Tutorial_1_SELECT_names.sql
+++ Tutorial_1_SELECT_names.sql
@@ -1,61 +1,93 @@
-
 -- Question 1
-SELECT name FROM world
-    WHERE name LIKE 'Y%'
+SELECT name
+FROM world
+WHERE name
+LIKE 'Y%'
 
 -- Question 2
-SELECT name FROM world
-    WHERE name LIKE '%Y'
+SELECT name
+FROM world
+WHERE name
+LIKE '%Y'
 
 -- Question 3
-SELECT name FROM world
-    WHERE name LIKE '%x%'
+SELECT name
+FROM world
+WHERE name
+LIKE '%x%'
 
 -- Question 4
-SELECT name FROM world
-    WHERE name LIKE '%land'
+SELECT name
+FROM world
+WHERE name
+LIKE '%land'
 
 -- Question 5
-SELECT name FROM world
-    WHERE name LIKE 'C%' AND name LIKE '%ia'
+SELECT name
+FROM world
+WHERE name
+LIKE 'C%'
+AND name
+LIKE '%ia'
 
 -- Question 6
-SELECT name FROM world
-    WHERE name LIKE '%oo%'
+SELECT name
+FROM world
+WHERE name
+LIKE '%oo%'
 
 -- Question 7
-SELECT name FROM world
-    WHERE name LIKE '%a%a%a%'
+SELECT name
+FROM world
+WHERE name
+LIKE '%a%a%a%'
 
 -- Question 8
-SELECT name FROM world
-    WHERE name LIKE '_t%' ORDER BY name
+SELECT name
+FROM world
+WHERE name
+LIKE '_t%'
+ORDER BY name
 
 -- Question 9
-SELECT name FROM world
-    WHERE name LIKE '%o__o%'
+SELECT name
+FROM world
+WHERE name
+LIKE '%o__o%'
 
 -- Question 10
-SELECT name FROM world
-    WHERE name LIKE '____'
+SELECT name
+FROM world
+WHERE name
+LIKE '____'
 
 
 -- Question 11
-SELECT name FROM world
-    WHERE name = capital
+SELECT name
+FROM world
+WHERE name = capital
 
 -- Question 12
-SELECT name FROM world
-    WHERE capital LIKE '%city'
+SELECT name
+FROM world
+WHERE capital
+LIKE '%city'
 
 -- Question 13
-SELECT capital, name FROM world
-    WHERE capital LIKE CONCAT('%', name, '%')
+SELECT capital, name
+FROM world
+WHERE capital
+LIKE CONCAT('%', name, '%')
 
 -- Question 14
-SELECT capital, name FROM world
-    WHERE capital LIKE CONCAT(name, '_%')
+SELECT capital, name
+FROM world
+WHERE capital
+LIKE CONCAT(name, '_%')
 
 -- Question 15
-SELECT name, REPLACE(capital, name, '') AS ext
-    FROM world WHERE capital LIKE CONCAT(name, '_%')
+SELECT name,
+       REPLACE(capital, name, '') AS ext
+FROM world
+WHERE capital
+LIKE CONCAT(name, '_%')
